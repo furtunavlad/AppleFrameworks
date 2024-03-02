cf64dc5dcdeed4471fba63bc546971e06c0cbebb
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-02 17:36:06 +0200

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Vlad Furtuna on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
